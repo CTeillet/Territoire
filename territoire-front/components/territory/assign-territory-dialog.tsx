6ea124933468ec93502e44205bef93e1a40f2d60
Teillet Corentin <teillet.corentin@neuf.fr>
2025-02-13 18:41:21 +0100

    assignation territoire

--- territoire-front/components/territory/assign-territory-dialog.tsx
+++ territoire-front/components/territory/assign-territory-dialog.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
