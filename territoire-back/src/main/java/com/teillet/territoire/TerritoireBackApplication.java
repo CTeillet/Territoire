da0a734f6bc849a36441b3fce5c9a261afb14937
Teillet Corentin <teillet.corentin@neuf.fr>
2025-02-26 14:14:02 +0100

    anomalie @Schedule

--- territoire-back/src/main/java/com/teillet/territoire/TerritoireBackApplication.java
+++ territoire-back/src/main/java/com/teillet/territoire/TerritoireBackApplication.java
@@ -2,8 +2,10 @@ package com.teillet.territoire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TerritoireBackApplication {
 
 	public static void main(String[] args) {
