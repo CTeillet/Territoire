c48e6098dcaa4343139e82bd6ef7efc5e8c02b40
Teillet Corentin <teillet.corentin@neuf.fr>
2025-08-02 14:02:48 +0200

    Ajout date export-excel-button.tsx

--- territoire-front/components/parameters/other-parameter/export-excel-button.tsx
+++ territoire-front/components/parameters/other-parameter/export-excel-button.tsx
@@ -12,7 +12,11 @@ export const ExportExcelButton = () => {
             const url = window.URL.createObjectURL(blob);
             const a = document.createElement("a");
             a.href = url;
-            a.download = "S-13.xlsx";
+            const today = new Date();
+            const formattedDate = today.getFullYear() + '-' + 
+                String(today.getMonth() + 1).padStart(2, '0') + '-' + 
+                String(today.getDate()).padStart(2, '0');
+            a.download = `S-13_${formattedDate}.xlsx`;
             document.body.appendChild(a);
             a.click();
             document.body.removeChild(a);
