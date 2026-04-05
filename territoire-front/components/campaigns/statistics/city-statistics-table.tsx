719025f40a15166647d4e32129d940f29425a4ea
Corentin TEILLET <teillet.corentin@gmail.com>
2026-04-05 19:29:05 +0200

    Ajout des statistiques par villes

--- territoire-front/components/campaigns/statistics/city-statistics-table.tsx
+++ territoire-front/components/campaigns/statistics/city-statistics-table.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CampaignStatistics } from "@/models/campaign-statistics";
-import { Table, TableHeader, TableBody, TableRow, TableHead } from "@/components/ui/table";
+import {Table, TableHeader, TableBody, TableRow, TableHead, TableCell} from "@/components/ui/table";
 import { TableWrapper } from "@/components/shared/ui/table-wrapper";
 import { TerritoryTypeRow } from "./territory-type-row";
 
